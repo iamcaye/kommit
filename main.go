2f149a6f18d83c2ade6d4b7183c126e4652aa393
Cayetano Biehler <cayetano.biehler@gmail.com>
2026-05-12 10:39:52 +0200

    feat: wire up main entrypoint

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"kommit/ai"
+	"kommit/git"
+	"kommit/picker"
+	"os"
+)
 
 func main() {
-	fmt.Println("kommit")
+	aiFlag := flag.String("ai", "", "AI backend to use: claude or codex (default: auto-detect)")
+	flag.Parse()
+
+	diff, err := git.GetStagedDiff(".")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	backend, err := ai.DetectBackend(*aiFlag)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	msgs, err := ai.Generate(diff, backend)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	message, err := picker.Pick(msgs)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if message == "" {
+		os.Exit(0)
+	}
+
+	if err := git.Commit(".", message); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
